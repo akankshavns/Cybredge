02d959eecf136ab7f507231c0b6163c530b0757d
Akankshavns <makanksha858@gmail.com>
2026-06-10 20:40:04 +0530

    finding factorial of number

--- daily_js_practice/index.js
+++ daily_js_practice/index.js
@@ -1,74 +1,75 @@
-// this script work for dark and light theme.
+function checkNum() {
+    let num = parseInt(document.getElementById("numbox").value);
+    let selection = document.getElementById("numberCatagory").value;
+    let Result = document.getElementsByClassName("result")[0];
 
-    let bgcolor = document.getElementsByClassName("Theme")[0];
-    function theme() {
-        let img = document.getElementById("mainimg");
-        let text = document.getElementsByClassName("bgco")[0];
+    if (selection == "Even_OR_Odd") {
 
-        if (img.src.includes("moon.jpg")) {
-            img.src = "image/sun.jpg";
-            text.innerHTML = "This is the dark Background theme";
-            text.style.color = "white";
-            bgcolor.style.background = "grey";
-            
+        if (num % 2 == 0) {
+            Result.innerHTML = "This is an Even Number.";
         } else {
-            img.src = "image/moon.jpg";
-            text.style.color = "black";
-            text.innerHTML = "This is the light Background theme";
-            bgcolor.style.background = "white";
+            Result.innerHTML = "This is an Odd Number.";
         }
-    }
 
-    // this script is finding the number catagory 
-    
-    
-    function checkNum(){
-        let num = document.getElementById("numbox").value;
-        let selection = document.getElementById("numberCatagory").value;
-        let Result = document.getElementsByClassName("result")[0];
-        let digit = 0;
-        if(selection == "Even_OR_Odd"){
-            if((num%2)==0){
-                Result.innerHTML = "This is an even Number.";
+    } 
+    else if (selection == "Armstrong") {
+
+        let len = num.toString().length;
+        let sum = 0;
+        let number = num;
+
+        while (num != 0) {
+            let digit = num % 10;
+            sum = sum + Math.pow(digit, len);
+            num = Math.floor(num / 10);
+        }
+
+        if (number == sum) {
+            Result.innerHTML = "This is an Armstrong Number.";
+        } else {
+            Result.innerHTML = "This is not an Armstrong Number.";
+        }
+
+    } 
+    else if (selection == "Prime") {
+
+        if (num <= 1) {
+            Result.innerHTML = "This is not a Prime Number.";
+        } else {
+            let isPrime = true;
+
+            for (let i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
-            else{
-                Result.innerHTML = "This is a odd Number.";
+
+            if (isPrime) {
+                Result.innerHTML = "This is a Prime Number.";
+            } else {
+                Result.innerHTML = "This is not a Prime Number.";
             }
+        }
+
+    } 
+    else if (selection == "factorial") {
+
+        let fact = 1;
+
+        if (num < 0) {
+            Result.innerHTML = "Factorial is not defined for negative numbers.";
+        }
+        else if (num == 0 || num == 1) {
+            Result.innerHTML = `The Factorial of ${num} is 1`;
         }
         else {
-            if (selection == "Armstrong") {
-                let len = num.toString().length;
-                let sum = 0;
-                let number = num;
-
-                while (num != 0) {
-                    let digit = num % 10;
-                    sum = sum + Math.pow(digit, len);
-                    num = Math.floor(num / 10);
-                }
-
-                if (number == sum) {
-                    Result.innerHTML = "This is an Armstrong Number.";
-                } else {
-                    Result.innerHTML = "This is not an Armstrong Number.";
-                }
+            for (let i = num; i >= 1; i--) {
+                fact = fact * i;
             }
-            else{
-                let i;
-                if(selection == "Prime" ){
-                    for(i = 2; i<=num; i++){
-                        if(num%i == 0){
-                            break;
-                        }
-                    }
 
-                }
-                if(i==num){
-                    Result.innerHTML = "This is Prime Number.";
-                }else{
-                    Result.innerHTML = "This is not a prime Number.";
-                }
-            }
-        
+            Result.innerHTML = `The Factorial of ${num} is ${fact}`;
         }
+
     }
+}
